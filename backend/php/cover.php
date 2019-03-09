f9ca4ddfe01679d5cc22e845748814a83efdc263
Susie6 <201830582210@mail.scut.edu.cn>
2019-03-09 21:13:11 +0800

    cover

--- backend/php/cover.php
+++ backend/php/cover.php
@@ -1,24 +1,24 @@
 <?php
 require_once("connect.php");
-Session_start();
+
 //接收姓名
-$name = htmlspecialchars($_SESSION['name'],ENT_QUOTES);
+$name = htmlspecialchars($_POST['name'],ENT_QUOTES);
 //接收手机号
-$phone = htmlspecialchars($_SESSION['phone'],ENT_QUOTES);
+$phone = htmlspecialchars($_POST['phone'],ENT_QUOTES);
 //接收年级
-$grade = htmlspecialchars($_SESSION['grade'],ENT_QUOTES);
+$grade = htmlspecialchars($_POST['grade'],ENT_QUOTES);
 //接收学院
-$college = htmlspecialchars($_SESSION['college'],ENT_QUOTES);
+$college = htmlspecialchars($_POST['college'],ENT_QUOTES);
 //接收宿舍
-$dorm = htmlspecialchars($_SESSION['dorm'],ENT_QUOTES);
+$dorm = htmlspecialchars($_POST['dorm'],ENT_QUOTES);
 //接收第一志愿
-$ChoiceOne = htmlspecialchars($_SESSION['ChoiceOne'],ENT_QUOTES);
+$ChoiceOne = htmlspecialchars($_POST['ChoiceOne'],ENT_QUOTES);
 //接收第二志愿
-$ChoiceTwo = htmlspecialchars($_SESSION['ChoiceTwo'],ENT_QUOTES);
+$ChoiceTwo = htmlspecialchars($_POST['ChoiceTwo'],ENT_QUOTES);
 //接收是否服从调剂
-$adjust = htmlspecialchars($_SESSION['adjust'],ENT_QUOTES);
+$adjust = htmlspecialchars($_POST['adjust'],ENT_QUOTES);
 //接收个人简介
-$introduction = htmlspecialchars($_SESSION['introduction'],ENT_QUOTES);
+$introduction = htmlspecialchars($_POST['introduction'],ENT_QUOTES);
 
 $sql = "update Attendee set grade = ?, college = ?, dorm = ?, ChoiceOne = ?, ChoiceTwo = ?, adjust = ?, introduction = ? where name = ? && phone = ?";
     $stmt = mysqli_prepare($con, $sql);
