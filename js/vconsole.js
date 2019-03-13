cc0f9a67d00a92d6ee95a21fc41116a4042db208
NancyLSH <1052567432@aq.com>
2019-03-14 03:44:02 +0800

    Update

--- js/vconsole.js
+++ js/vconsole.js
@@ -14,12 +14,39 @@ Unless required by applicable law or agreed to in writing, software distributed 
  */
 
 // global
-import './lib/symbol.js';
+//import './lib/symbol.js';
 
 // classes
-import VConsole from './core/core.js';
-import VConsolePlugin from './lib/plugin.js';
+//import VConsole from './core/core.js';
+//import VConsolePlugin from './lib/plugin.js';
 
 // export
-VConsole.VConsolePlugin = VConsolePlugin;
-export default VConsole;
+//VConsole.VConsolePlugin = VConsolePlugin;
+//export default VConsole;
+
+
+function checkintro_login()
+{
+    let name = document.getElementById("name_input").value  
+    let tel = document.getElementById("tel_input").value  
+    $("#siginup").css("display","none")
+    $("#introduction").css("display","none")
+    document.getElementById("name").innerText="姓名：";
+    document.getElementById("tel").innerText="电话：";
+    $("#name_input").css("display","block")
+    $("#tel_input").css("display","block")
+    $.ajax({
+        type:"POST",
+        url:"./api/action.php?method=query",
+        data:{'name':name,'tel':tel},
+        success:function(errmsg){
+            if(errmsg != "ok"){
+                console.log(errmsg)
+            }else{
+                console.log("我好困呜呜呜")
+
+            }
+        }
+
+    }) 
+}
