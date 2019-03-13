981561e4c467d70d43fe993780f00311cff5ddd9
Hikoyan <huanghongyan324@gmail.com>
2019-03-14 00:17:02 +0800

    swiper basically completed

--- js/main.js
+++ js/main.js
@@ -7,17 +7,23 @@ $(function(){
     article[0]="部门简介一长串";
     // for(var i =1;i<depa.length;i++){
     // }
-    var showSwiper = new Swiper('.swiper-container',{
-        // initialSlide : 21,
-        // direction: 'horizontal',
-        grabCursor : true,
-        // autoheight: true,
-        loop: true,
-        eventTarget : 'container',
-        releaseFormElements : false,
-        watchActiveIndex : true,
-        visibilityFullFit : true,
-        keyboardControl : true,
-        mousewheelControl : true,
-        });
+    // window.onload=function(){
+      // var showSwiper = new Swiper('.swiper-container',{
+      //   // initialSlide : 21,
+      //   // direction: 'horizontal',
+      //   grabCursor : true,
+      //   // autoheight: true,
+      //   loop: true,
+      //   eventTarget : 'container',
+      //   releaseFormElements : false,
+      //   watchActiveIndex : true,
+      //   visibilityFullFit : true,
+      //   keyboardControl : true,
+      //   mousewheelControl : true,
+      //   navigation: {
+      //       nextEl: '.swiper-button-next',
+      //       prevEl: '.swiper-button-prev',
+      //     },
+      //   });
+      // }
 }) 
