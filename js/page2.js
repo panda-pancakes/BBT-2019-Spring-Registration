8321dfb34daa968113ee30901f6d74a8a9bf6974
Hikoyan <huanghongyan324@gmail.com>
2019-03-18 01:23:34 +0800

    renew css

--- js/page2.js
+++ js/page2.js
@@ -8,26 +8,26 @@ $(function () {
     //js混用
     function oninput(){
         $("#name").bind('input propertychange', function() { 
-            $("#name").val().replace("/[\'\"\\\/\b\f\n\r\t]/g","");
-            $("#name").val().replace("(/[\@\#\$\%\^\&\*\{\}\:\"\L\<\>\?","");
+            $("#name").val().replace(/[\'\"\\\/\b\f\n\r\t]/,'');
+            $("#name").val().replace(/\@\#\$\%\^\&\*\{\}\:\\L\<\>\?}/,'');
             $("#name").attr("value",$("#name").val());
             cc();
            });
         $("#dorm").bind('input propertychange', function(){
-            $("#dorm").val().replace("/[\'\"\\\/\b\f\n\r\t]/g","");
-            $("#dorm").val().replace("(/[\@\#\$\%\^\&\*\{\}\:\"\L\<\>\?","");
+            $("#dorm").val().replacereplace(/[\'\"\\\/\b\f\n\r\t]/,'');
+            $("#dorm").val().replace(/\@\#\$\%\^\&\*\{\}\:\\L\<\>\?}/,'');
             $("#dorm").attr("value",$("#dorm").val());
             cc();
         })
         $("#tel").bind('input propertychange', function(){
-            $("#tel").val().replace("/[\'\"\\\/\b\f\n\r\t]/g","");
-            $("#tel").val().replace("(/[\@\#\$\%\^\&\*\{\}\:\"\L\<\>\?","");
+            $("#tel").val().replacereplace(/[\'\"\\\/\b\f\n\r\t]/,'');
+            $("#tel").val().replace(/\@\#\$\%\^\&\*\{\}\:\\L\<\>\?}/,'');
             $("#tel").attr("value",$("#tel").val());
             cc();
         })
         $("#introduction").bind('input propertychange', function(){
-            $("#introduction").val().replace("/[\'\"\\\/\b\f\n\r\t]/g","");
-            $("#introduction").val().replace("(/[\@\#\$\%\^\&\*\{\}\:\"\L\<\>\?","");
+            $("#introduction").val().replacereplace(/[\'\"\\\/\b\f\n\r\t]/,'');
+            $("#introduction").val().replace(/\@\#\$\%\^\&\*\{\}\:\\L\<\>\?}/,'');
             $("#introduction").attr("value",$("#introduction").val());
             cc();
         })    
@@ -40,12 +40,14 @@ $(function () {
         $("#bbt").hide();
         // $("#check_box").show();
         $("#check_box").css({
-            "margin-top": "1%",
-            "display": "table",
+            "margin-top": "35%",
+            // "display": "table",
+            "margin-left": "20%",
+            "display": "block",
             "vertical-align": " middle",
             "text-align": "center",
-            "padding-left": "10%",
-            "padding-top": "25%",
+            // "padding-left": "10%",
+            // "padding-top": "25%",
             "max-width": "200%",
         });
         $("#prev").css({
@@ -82,10 +84,11 @@ $(function () {
             tel,
         });
         console.log(info);
+        cc();
+        if(cc()==true){
         $.post("./api/action.php?method=query", info, function (data, status) {
             if (status == "success") {
                 if (data.status == "failed") {
-                    $("#233").show(); //直接报名按钮
                     var missing = new RegExp('Missing');
                     var telephone = new RegExp('telephone');
                     if (missing.test(data.errmsg)) {
@@ -103,6 +106,7 @@ $(function () {
                     } else {
                         attention();
                         $("#attention").text("查询成功");
+                        $("#cover_user").show(); //修改按钮
                         console.log(data.info);
                     }
                 } else {
@@ -111,6 +115,7 @@ $(function () {
                 }
             }
         })
+    }
     })
 
     //覆盖
@@ -123,7 +128,9 @@ $(function () {
             name,
             tel,
         });
-        $.post("/api/action.php?method=query", info, function (data, status) {
+        cc();
+        if(cc()==true){
+            $.post("/api/action.php?method=query", info, function (data, status) {
             if (status == "success") {
                 if (data.status == "failed") {
                     var missing = new RegExp('Missing');
@@ -142,17 +149,17 @@ $(function () {
                         $("#attention").text("不好意思，没有您的报名信息哦");
                     } else {
                         attention();$("#attention").text("系统繁忙，请稍后再试");
-                       
-                        cover();
                         $("#successbox:first-child").show();
                         // console.log(data.info);
                     }
-                } else {
+                }else{
+                    cover();
                     attention();
-                     $("#attention").text("查询成功");
+                    $("#attention").text("查询成功");    
                 }
             }
         })
+    }
     })
     console.log("------138----路过覆盖css函数上空 --------");
 
@@ -161,12 +168,6 @@ $(function () {
         $("#successbox:first-child").hide();
         $("#successbox").css({
             "visibility": "visible",
-            "display": "table-cell",
-            "z-index": "10",
-            "max-width": "100% ",
-            "color": " #707070",
-            "font-size": "1em",
-            "text-align": "center"
         });
     }
     console.log("----152------前端检查字符函数上空 --------");
@@ -238,7 +239,7 @@ $(function () {
         $("#attention").show();
         $("#cover_user").show();
         $("#attention").css({
-            "display": "table-cell",
+            "display": "block",
         });
     }
     console.log("------194----check上空 --------");
@@ -312,59 +313,63 @@ $(function () {
 
     //前端提示 msg 
     //所有部门 数组
-    var depa = new Array();
-    depa[0] = "技术部-代码组";
-    depa[1] = "技术部-设计组";
-    depa[2] = "技术部（北校专业）";
-    depa[3] = "策划推广部";
-    depa[4] = "编辑部-原创写手";
-    depa[5] = "编辑部-摄影";
-    depa[6] = "编辑部-可视化设计";
-    depa[7] = "视觉设计部";
-    depa[8] = "视频部-策划导演";
-    depa[9] = "视频部-摄影摄像";
-    depa[10] = "视频部-剪辑特效";
-    depa[11] = "外联部";
-    depa[12] = "节目部-国语组";
-    depa[13] = "节目部-英语组";
-    depa[14] = "节目部-粤语组";
-    depa[15] = "人力资源部";
-    depa[16] = "综合管理部-行政管理";
-    depa[17] = "综合管理部-物资财物";
-    depa[18] = "综合管理部-撰文记者";
-    depa[19] = "综合管理部-摄影记者";
-    depa[20] = "产品运营部（北校专业）";
-
     var major = new Array();
-    major[0] = "机械与汽车工程学院";
-    major[1] = "建筑学院";
-    major[2] = "土木与交通学院";
-    major[3] = "电子与信息学院";
-    major[4] = "材料科学与工程学院";
-    major[5] = "化学与化工学院";
-    major[6] = "轻工科学与工程学院";
-    major[7] = "食品科学与工程学院";
-    major[8] = "数学学院";
-    major[9] = "物理与光电学院";
-    major[10] = "经济与贸易学院";
-    major[11] = "自动化科学与工程学院";
-    major[12] = "计算机科学与工程学院";
-    major[13] = "电力学院";
-    major[14] = "生物科学与工程学院"
-    major[15] = "环境与能源学院";
-    major[16] = "软件学院";
-    major[17] = "工商管理学院";
-    major[18] = "公共管理学院";
-    major[19] = "马克思主义学院";
-    major[20] = "外国语学院";
-    major[21] = "法学院";
-    major[22] = "新闻与传播学院";
-    major[23] = "艺术学院";
-    major[24] = "体育学院";
-    major[25] = "设计学院";
-    major[26] = "医学院";
-    major[27] = "国际教育学院";
-
+    var depa = new Array();
+    addarray();
+    function addarray(){
+        depa[0] = "技术部-代码组";
+        depa[1] = "技术部-设计组";
+        depa[2] = "技术部（北校专业）";
+        depa[3] = "策划推广部";
+        depa[4] = "编辑部-原创写手";
+        depa[5] = "编辑部-摄影";
+        depa[6] = "编辑部-可视化设计";
+        depa[7] = "视觉设计部";
+        depa[8] = "视频部-策划导演";
+        depa[9] = "视频部-摄影摄像";
+        depa[10] = "视频部-剪辑特效";
+        depa[11] = "外联部";
+        depa[12] = "节目部-国语组";
+        depa[13] = "节目部-英语组";
+        depa[14] = "节目部-粤语组";
+        depa[15] = "人力资源部";
+        depa[16] = "综合管理部-行政管理";
+        depa[17] = "综合管理部-物资财物";
+        depa[18] = "综合管理部-撰文记者";
+        depa[19] = "综合管理部-摄影记者";
+        depa[20] = "产品运营部（北校专业）";
+    
+        major[0] = "机械与汽车工程学院";
+        major[1] = "建筑学院";
+        major[2] = "土木与交通学院";
+        major[3] = "电子与信息学院";
+        major[4] = "材料科学与工程学院";
+        major[5] = "化学与化工学院";
+        major[6] = "轻工科学与工程学院";
+        major[7] = "食品科学与工程学院";
+        major[8] = "数学学院";
+        major[9] = "物理与光电学院";
+        major[10] = "经济与贸易学院";
+        major[11] = "自动化科学与工程学院";
+        major[12] = "计算机科学与工程学院";
+        major[13] = "电力学院";
+        major[14] = "生物科学与工程学院"
+        major[15] = "环境与能源学院";
+        major[16] = "软件学院";
+        major[17] = "工商管理学院";
+        major[18] = "公共管理学院";
+        major[19] = "马克思主义学院";
+        major[20] = "外国语学院";
+        major[21] = "法学院";
+        major[22] = "新闻与传播学院";
+        major[23] = "艺术学院";
+        major[24] = "体育学院";
+        major[25] = "设计学院";
+        major[26] = "医学院";
+        major[27] = "国际教育学院";
+    
+    
+    }
     //select的option value循环
     function selector() {
         for (var i = 0; i <= 20; i++) {
