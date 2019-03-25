76a44aec41866d97acfb5109bc1f177cec19d8fc
Joshua Astray <JoshuaSRKF@gmail.com>
2019-03-26 02:10:14 +0800

    changed string type in db

--- install.php
+++ install.php
@@ -6,9 +6,9 @@ if ($con->connect_error) {
 	echo("<b>Failed to access database: </b>" . $con->connect_error);
 
 } else {
-	$con->query("CREATE TABLE `" . DB_NAME . "`.`application` ( `name` TEXT NOT NULL , `sex` CHAR NOT NULL , `tel` TEXT NOT NULL , `grade` INT NOT NULL , `college` INT NOT NULL , `dorm` TEXT NULL , `department` INT NOT NULL , `alternative` INT NULL , `adjustment` TEXT NOT NULL , `introduction` TEXT NULL , `timestamp` TIMESTAMP NOT NULL , `information` TEXT NULL , `note` TEXT NULL ) ENGINE = InnoDB;");
+	$con->query("CREATE TABLE `" . DB_NAME . "`.`application` ( `name` VARCHAR NOT NULL , `sex` CHAR NOT NULL , `tel` VARCHAR NOT NULL , `grade` INT NOT NULL , `college` INT NOT NULL , `dorm` VARCHAR NULL , `department` INT NOT NULL , `alternative` INT NULL , `adjustment` VARCHAR NOT NULL , `introduction` VARCHAR NULL , `timestamp` TIMESTAMP NOT NULL , `information` VARCHAR NULL , `note` VARCHAR NULL ) ENGINE = InnoDB;");
 
-	$con->query("CREATE TABLE `" . DB_NAME . "`.`admin` ( `username` TEXT NOT NULL , `password` TEXT NOT NULL , `permission` INT NOT NULL ) ENGINE = InnoDB;");
+	$con->query("CREATE TABLE `" . DB_NAME . "`.`admin` ( `username` VARCHAR NOT NULL , `password` VARCHAR NOT NULL , `permission` INT NOT NULL ) ENGINE = InnoDB;");
 
 	/* TO-DO: 建立默认的管理员账号 */
 
