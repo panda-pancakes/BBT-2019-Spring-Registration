cc3eee6671b9f71a8c17b32e13b7e3a23a3ba918
NancyLSH <1052567432@aq.com>
2019-03-26 18:59:39 +0800

    add something.

--- api/checker.php
+++ api/checker.php
@@ -10,3 +10,6 @@ registerChecker("tel", function($var) {
 registerChecker("sex", function($var) {
 	return $var === "M" or $var === "F";
 });
+registerChecker("dorm",function($var){
+	return $var[0] == 'C' && strlen($var) == 7;
+});
