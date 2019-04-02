5f5b7555b1bfd56cda2b5f8ed6b498ca456b5859
Joshua Astray <JoshuaSRKF@gmail.com>
2019-04-02 21:51:13 +0800

    Update checker.php

--- api/checker.php
+++ api/checker.php
@@ -7,9 +7,26 @@ registerChecker("tel", function($var) {
 	return is_numeric($var) && $var[0] == 1 && strlen($var) == 11;
 });
 
-// registerChecker("sex", function($var) {
-// 	return $var === "M" or $var === "F";
-// });
-// registerChecker("dorm",function($var){
-// 	return $var[0] == 'C' && strlen($var) == 7;
-// });
+registerChecker("sex", function($var) {
+ 	return $var === "M" or $var === "F";
+});
+
+registerChecker("department", function($var) {
+ 	return is_numeric($var) && $var >= 1 && $var <= 21;
+});
+
+registerChecker("alternative", function($var) {
+ 	return is_numeric($var) && $var >= 0 && $var <= 21;
+});
+
+registerChecker("college", function($var) {
+ 	return is_numeric($var) && $var >= 1 && $var <= 28;
+});
+
+registerChecker("dorm", function($var){
+ 	return ($var[0] == 'C' || $var[0] == 'c') && strlen($var) >= 5 && strlen($var) <= 9;
+});
+
+registerChecker("adjustment", function($var) {
+ 	return $var === true || $var === false;
+});
